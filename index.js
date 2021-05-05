8a997d4d58408cc7454c6f65cdb15537a407a050
Christian Bermeo <czaudiovisual@hotmail.com>
2021-05-04 23:16:39 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// const totalBatteries = (battery, batches) => battery + batches;
+// console.log(batteryBatches.reduce(totalBatteries));
+
+
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5].reduce(function(totalBatteries, batteries) { 
+//     return batteries + totalBatteries}, 0);
+// console.log(batteryBatches);
+
+
+const totalBatteries = batteryBatches.reduce(function(batches, batteries){
+	batches = batches + batteries;
+    return batches;
+}, 0);
+console.log(totalBatteries);
